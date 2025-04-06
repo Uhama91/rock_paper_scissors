310eba791d8c2d4063289950534345f350d8f4db
uhama91 <harounjean@hotmail.fr>
2025-04-06 07:25:51 +0200

    coded the choice of the computer

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+const getComputerChoice = () => {
+    //randomly decide console rock, paper or scissors
+    let randomNumber = Math.floor(Math.random()*3)
+    if(randomNumber===0){
+        return "Rock";
+    }else if(randomNumber===1) {
+        return "Paper";
+    } else {
+        return "scissors"
+    }
+}
