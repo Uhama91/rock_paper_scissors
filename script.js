29600f9b95268987da56f95e34f34a38dd12e95b
uhama91 <harounjean@hotmail.fr>
2025-04-06 09:23:32 +0200

    code the function getHumanChoice but doesn't return value

--- script.js
+++ script.js
@@ -2,10 +2,52 @@ const getComputerChoice = () => {
     //randomly decide console rock, paper or scissors
     let randomNumber = Math.floor(Math.random()*3)
     if(randomNumber===0){
-        return "Rock";
+        return "rock";
     }else if(randomNumber===1) {
-        return "Paper";
+        return "paper";
     } else {
         return "scissors"
     }
 }
+
+// user choice
+let choice = prompt("Rock, paper or scissors?");
+
+const getHumanChoice = (choice) => {
+
+let computerChoice = getComputerChoice();
+
+    switch(choice){
+
+        case choice.toLowerCase==="rock":
+            if(computerChoice==="rock"){
+                alert("DRAW");
+            } else if(computerChoice==="paper"){
+                alert("You WIN!");
+            } else{
+                alert("You LOOSE!");
+            }
+            break;
+
+        case choice.toLowerCase==="paper":
+            if(computerChoice==="rock"){
+                alert("You LOOSE!");
+            } else if(computerChoice==="paper"){
+                alert("DRAW");
+            } else{
+                alert("You WIN!");
+            }
+            break;
+
+            case choice.toLowerCase==="scissors":
+            if(computerChoice==="rock"){
+                alert("You LOOSE!");
+            } else if(computerChoice==="paper"){
+                alert("You WIN!");
+            } else{
+                alert("DRAW");
+            }
+            break;
+    }
+}
+
