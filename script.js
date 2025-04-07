eb1cff73d39c85d4cfb709df436364978e0c6dce
uhama91 <harounjean@hotmail.fr>
2025-04-07 05:33:37 +0200

    add alert in getComputerCHoice funct to make visible what it's playing

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ const getComputerChoice = () => {
     //randomly decide console rock, paper or scissors
     let randomNumber = Math.floor(Math.random()*3)
     if(randomNumber===0){
+        alert("Computer choose rock");
         return "rock";
     }else if(randomNumber===1) {
+        alert("Computer choose paper");
         return "paper";
     } else {
+        alert("Computer choose scissors");
         return "scissors"
     }
 }
@@ -17,12 +20,12 @@ const getHumanChoice = () => {
 
 let computerChoice = getComputerChoice();
 
-    switch(choicePlayer.toLowerCase){
+    switch(choicePlayer){
         case "rock":
             if(computerChoice==="rock"){
                 alert("DRAW, no looser no winner");
             } else if(computerChoice==="paper"){
-                alert("You LOSOE! Paper wraps rock...");
+                alert("You LOOSE! Paper wraps rock...");
             } else{
                 alert("You WIN! Rock crashes scissors!");
             }
