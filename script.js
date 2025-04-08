be7d2c15f0e9ba94bf328633c6124cb195810d3e
uhama91 <harounjean@hotmail.fr>
2025-04-08 05:33:56 +0200

    prompt was declare in the global scope, I moved it in the getHumannChoice funct

--- script.js
+++ script.js
@@ -14,43 +14,87 @@ const getComputerChoice = () => {
 }
 
 // user choice
-let choicePlayer = prompt("Rock, paper or scissors?");
+let playerPrompt = prompt("Rock, paper or scissors?");
 
-const getHumanChoice = () => {
+// what player choose and alert the choice
+const getHumanChoice = (playerChoice) => {
+    playerChoice = playerPrompt;
 
-let computerChoice = getComputerChoice();
-
-    switch(choicePlayer){
+    switch(playerChoice){
         case "rock":
-            if(computerChoice==="rock"){
-                alert("DRAW, no looser no winner");
-            } else if(computerChoice==="paper"){
-                alert("You LOOSE! Paper wraps rock...");
-            } else{
-                alert("You WIN! Rock crashes scissors!");
-            }
+            alert("You played rock");
             break;
-
+        
         case "paper":
-            if(computerChoice==="rock"){
-                alert("You WIN! Paper wraps rock!");
-            } else if(computerChoice==="paper"){
-                alert("DRAW, no looser no winner");
-            } else{
-                alert("You LOOSE! Scissors cut paper...");
-            }
+            alert("You played paper");
             break;
-
-            case "scissors":
-            if(computerChoice==="rock"){
-                alert("You LOOSE! Rock crashes scissors...");
-            } else if(computerChoice==="paper"){
-                alert("You WIN! Scissors cut paper!");
-            } else{
-                alert("DRAW, no looser no winner");
-            }
+        
+        case "scissors":
+            alert("You played scissors");
             break;
     }
+
 }
 
-getHumanChoice();
+let humanScore = 0;
+let computerScore = 0;
+
+//One round of the game nd counts a point
+const playRound =(humanChoice, computerChoice)=>{
+    
+    humanChoice = getHumanChoice();
+    computerChoice = getComputerChoice();
+        
+    switch(humanChoice){
+                case "rock":
+                    if(computerChoice==="rock"){
+                        alert("DRAW, no looser no winner");
+                    } else if(computerChoice==="paper"){
+                        alert("You LOOSE! Paper wraps rock...");
+                        return computerScore += 1;
+                    } else{
+                        alert("You WIN! Rock crashes scissors!");
+                        return humanScore += 1;
+                    }
+                    break;
+        
+                case "paper":
+                    if(computerChoice==="rock"){
+                        alert("You WIN! Paper wraps rock!");
+                        return humanScore += 1;
+                    } else if(computerChoice==="paper"){
+                        alert("DRAW, no looser no winner");
+                    } else{
+                        alert("You LOOSE! Scissors cut paper...");
+                        return computerScore += 1;
+                    }
+                    break;
+        
+                    case "scissors":
+                    if(computerChoice==="rock"){
+                        alert("You LOOSE! Rock crashes scissors...");
+                        return computerScore += 1;
+                    } else if(computerChoice==="paper"){
+                        alert("You WIN! Scissors cut paper!");
+                        return humanScore += 1;
+                    } else{
+                        alert("DRAW, no looser no winner");
+                    }
+                    break;
+            }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+    
+//logic of the game
+
+const playGame = (oneParty) => {
+    for(let i=0; i<=5; i++){
+        oneParty = playRound(humanSelection, computerSelection);
+    }
+    return oneParty;
+}
+
+playGame();
