8365d1ca97e4fd542deed11e165db7833ddec141
uhama <harounjean@hotmail.fr>
2025-05-15 06:27:56 +0200

    put in each eventListener the mechanic of the game, project is done

--- script.js
+++ script.js
@@ -6,45 +6,76 @@ const paraComputer = document.querySelector('#paraComputerChoice');
 const paraPlayer = document.querySelector('#paraPlayerChoice');
 const roundResult = document.querySelector('#roundResult');
 const roundPara = document.querySelector('#round');
+const score = document.querySelector('#score');
+const finalScore = document.querySelector('#finalScore');
 
 let round = 0;
 let humanScore = 0;
 let computerScore = 0;
 
 const clickRock = rockBtn.addEventListener('click', () => {
-    paraPlayer.textContent = 'You played rock';
-    playRound('rock', getComputerChoice());
     
     if(round === 5){
+        if(humanScore>computerScore){
+            finalScore.textContent = `Well play! You WIN!!`;
+        } else if(computerScore>humanScore){
+            finalScore.textContent = `What a pitty! You LOSE!!`;
+        } else {          
+            finalScore.textContent = `Neither winner, neither winner, RAW!`;
+
+        }
         return
         //display result
     } else {
+        paraPlayer.textContent = 'You played rock';
+        playRound('rock', getComputerChoice());
         round++
         roundPara.textContent = `round ${round}`;
+        score.textContent = `Player'score: ${humanScore} - Computer'score: ${computerScore}`;
     }
 });
-const clickPaper = paperBtn.addEventListener('click', () => {
-    paraPlayer.textContent = 'You played paper';
-    playRound('paper', getComputerChoice());
+    const clickPaper = paperBtn.addEventListener('click', () => {
     
     if(round === 5){
+        if(humanScore>computerScore){
+            finalScore.textContent = `Well play! You WIN!!`;
+        } else if(computerScore>humanScore){
+            finalScore.textContent = `What a pitty! You LOSE!!`;
+        } else {          
+            finalScore.textContent = `Neither winner, neither winner, RAW!`;
+
+        }
         return
-    } else {
+        } else {
+        paraPlayer.textContent = 'You played paper';
+        playRound('paper', getComputerChoice());
+
         round++
         roundPara.textContent = `round ${round}`;
-    }
-});
-const clickScissors = scissorsBtn.addEventListener('click', () => {
-    paraPlayer.textContent = 'You played scissors';
-    playRound('scissors', getComputerChoice());
+        score.textContent = `Player'score: ${humanScore} - Computer'score: ${computerScore}`;
+
+        }
+    });
+    const clickScissors = scissorsBtn.addEventListener('click', () => {
     
     if(round === 5){
+        if(humanScore>computerScore){
+            finalScore.textContent = `Well play! You WIN!!`;
+        } else if(computerScore>humanScore){
+            finalScore.textContent = `What a pitty! You LOSE!!`;
+        } else {          
+            finalScore.textContent = `Neither winner, neither winner, RAW!`;
+
+        }
         return
-    } else {
+        } else {
+        paraPlayer.textContent = 'You played scissors';
+        playRound('scissors', getComputerChoice());
         round++
         roundPara.textContent = `round ${round}`;
-    }
-});
+        score.textContent = `Player'score: ${humanScore} - Computer'score: ${computerScore}`;
+        }
+    });
 
 const getComputerChoice = () => {
     //randomly decide console rock, paper or scissors
